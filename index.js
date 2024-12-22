3fd89de4f6996f6530cb88ec37d486d91e15690c
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-22 20:09:39 +0600

    create account api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const User = require("./models/user.model")
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +28,56 @@ app.use(cors({
 app.get('/', (req, res) => {
     res.send("Noteify server is running")
 })
+
+// Create Account
+app.post('/create-account', async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    if(!fullName) {
+        return res
+        .status(400)
+        .json({ error: true, message: "Full Name is required" });
+    }
+
+    if(!email) {
+        return res.status(400).json({ error: true, message: "Email is required" });
+    }
+
+    if(!password) {
+        return res 
+        .status(400)
+        .json({ error: true, message: "Password is required"});
+    }
+
+    const isUser = await User.findOne({ email: email });
+
+    if(isUser) {
+        return res.json({
+            error: true,
+            message: "User already exist",
+        })
+    }
+
+    const user = new User ({
+        fullName,
+        email,
+        password,
+    })
+
+    await user.save();
+
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "36000m",
+    })
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "Registration Successful!",
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`This server is running on port: ${port}`);
