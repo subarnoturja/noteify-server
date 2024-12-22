775faea463e9eb2928b7c61c6c40ea2b5e862031
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-22 22:03:37 +0600

    all notes api added

--- index.js
+++ index.js
@@ -212,6 +212,30 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
     }
 })
 
+// All Note
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+    const { user } = req.user;
+
+    try{
+        const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1})
+
+        return res 
+        .json({ 
+            error: false, 
+            notes, 
+            message: "All Notes retrieved successfully!",
+        })
+    }
+    catch(error) {
+        return res 
+        .status(500)
+        .json({
+            error: true,
+            message: "Internal Server Error",
+        })
+    }
+})
+
 app.listen(port, () => {
     console.log(`This server is running on port: ${port}`);
 })
