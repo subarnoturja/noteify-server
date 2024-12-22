3fbe02da8853b55a7e3b5172e2b593319ea75b42
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-22 23:34:32 +0600

    updated isPinned api is added

--- index.js
+++ index.js
@@ -268,6 +268,39 @@ app.delete('/delete-note/:noteId', authenticateToken, async (req, res) => {
     }
 })
 
+// Update isPinned value
+app.put('/update-note-pinned/:noteId', authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id })
+
+        if(!note) { 
+            return res 
+            .status(404)
+            .json({ error: true, message: "Not not found" });
+        }
+
+        note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully!",
+        })
+    }
+    catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal server error",
+        })
+    }
+})
+
 app.listen(port, () => {
     console.log(`This server is running on port: ${port}`);
 })
