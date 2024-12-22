22fda8dc0e035040347b0f5dead00398f8eae741
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-22 21:58:32 +0600

    edit note api added

--- index.js
+++ index.js
@@ -168,6 +168,50 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     }
 })
 
+// Edit Note
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+
+    if(!title && !content && !tags){
+        return res 
+        .status(400)
+        .json({ error: true, message: "No changes provided" });
+    }
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+        if(!note) {
+            return res 
+            .status(404)
+            .json({ error: true, message: "Note not found" });
+        }
+
+        if(title) note.title = title;
+        if(content) note.content = content;
+        if(tags) note.tags = tags;
+        if(isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully",
+        })
+    }
+    catch(error){
+        return res 
+        .status(500)
+        .json({
+            error: true,
+            message: "Internal Server error",
+        })
+    }
+})
+
 app.listen(port, () => {
     console.log(`This server is running on port: ${port}`);
 })
